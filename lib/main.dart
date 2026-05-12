0936988b6fd6a4024154e1d04bccc2367f5e2453
Denzel Xavier <dionisiusdenzel@gmail.com>
2026-05-12 09:14:01 +0700

    Supabase connection

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'features/games/pages/home_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
@@ -10,6 +11,11 @@ void main() async {
   await dotenv.load(fileName: ".env");
 
   await Hive.initFlutter();
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
 
   // await Hive.openBox('wishlist');
   // await Hive.openBox('ratings');
